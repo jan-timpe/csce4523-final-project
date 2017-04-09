ebfe38ffa5d4b0be41b70301828f956ee58f508c
Jan Timpe <jantimpe@email.uark.edu>
2017-04-09 09:00:21 -0500

    Added url mapping, template for about page

--- main.py
+++ main.py
@@ -24,5 +24,9 @@ def cleanup_tables():
 def home():
     return render_template('home.html', title='Welcome')
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
 if __name__ == "__main__":
     app.run()
