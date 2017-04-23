7d0dcf8622e62cbc65e412eafd3090e69be42c69
Jan Timpe <jantimpe@email.uark.edu>
2017-04-23 10:12:02 -0500

    Commenting and refactoring

--- student/views.py
+++ student/views.py
@@ -1,12 +1,16 @@
 from flask import abort, Blueprint, render_template, redirect, request, url_for
-from .models import Student, StudentEnrollment
-from .forms import StudentForm
 from course.models import Course
 from department.models import Department
+from .forms import StudentForm
+from .models import Student, StudentEnrollment
 from playhouse.shortcuts import cast
 
+# Create the module blueprint
+# This is imported in main.py
 student = Blueprint('student', __name__, template_folder='templates')
 
+# a helper method
+# TODO: this is repeated in every module, move to a helper functions file
 def get_object_or_404(model, *args):
     try:
         return model.get(*args)
@@ -14,6 +18,8 @@ def get_object_or_404(model, *args):
         abort(404)
 
 
+# Fetches a list of every student in the database
+# Pass a url parameter [ ?q=some-search-param ] to filter results
 @student.route('/')
 def list():
     search_param = request.args.get('q')
@@ -35,18 +41,25 @@ def list():
         search=search_param
     )
 
+# Fetch a single student object by student id
+# Throws a 404 error if the student is not found in the database
 @student.route('/<student_id>')
 def get(student_id):
     student = get_object_or_404(Student, Student.student_id == student_id)
+
     return render_template(
         'student/details.html',
         title=student.first_name+" "+student.last_name,
         student=student
     )
 
+# Create a new student object
+# Handles both GET and POST requests
+# Redirects user to student.get() upon successful submission
 @student.route('/create', methods=['GET', 'POST'])
 def create():
     form = StudentForm()
+
     if form.validate_on_submit():
         student = Student()
         form.populate_obj(student)
@@ -60,10 +73,14 @@ def create():
         form=form
     )
 
+# Edit an existing student
+# Throws 404 if student lookup fails
+# Redirects to student.get() on success
 @student.route('/<student_id>/edit', methods=['GET', 'POST'])
 def edit(student_id):
     student = get_object_or_404(Student, Student.student_id == student_id)
     form  = StudentForm(obj=student)
+
     if form.validate_on_submit():
         form.populate_obj(student)
         student.save()
@@ -76,6 +93,10 @@ def edit(student_id):
         form=form
     )
 
+# Deletes the student
+# FIXME: this allows for deleting data without confirmation through a GET request
+# FIXME: add CSRF protection and DELETE request requirement
+# FIXME: do this through some sort of form with confirmation
 @student.route('/<student_id>/delete')
 def delete(student_id):
     student = get_object_or_404(Student, Student.student_id == student_id)
@@ -84,6 +105,7 @@ def delete(student_id):
 
     return redirect(url_for('student.list'))
 
+# Retrieve a list of courses that the student is not already enrolled in
 @student.route('/<student_id>/enroll', methods=['GET'])
 def enroll(student_id):
     search_param = request.args.get('q')
@@ -115,20 +137,15 @@ def enroll(student_id):
         search=search_param
     )
 
+# Handles POST requests to the student enrollment form
 @student.route('/<student_id>/enroll', methods=['POST'])
 def enroll_student(student_id):
     selected_courses = request.form.getlist('selected_courses')
-    print(selected_courses)
-
     student = get_object_or_404(Student, Student.student_id == student_id)
 
-    # this is not a good method. slow, not ACID compliant; too many queries, potential to fail mid-query and throw an unnecessary 404
-
-    # this is a much better method; ensures only courses that exist in the database are being added.
+    # FIXME: kill the operation if a nonexistant course id is found in the form
+    # FIXME: use the database.commit() functionality here instead
     for course in Course.select().where(Course.id.in_(selected_courses)):
-        # course = get_object_or_404(Course, Course.id == course_id)
-        print(course.name)
-        print(student.full_name())
         enrollment = StudentEnrollment(
             student = student.id,
             course = course.id
@@ -136,6 +153,8 @@ def enroll_student(student_id):
         enrollment.save()
     return redirect(student.absolute_url())
 
+# Delete the entry from the StudentEnrollment table
+# FIXME: add CSRF protection, only accept DELETE requests through a form
 @student.route('/<student_id>/drop/<enrollment_id>')
 def drop_course(student_id, enrollment_id):
     student = get_object_or_404(Student, Student.student_id == student_id)
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,9 @@
 from main import app
 
+# Simply import and start the app
+# This becomes gunicorn's application entrypoint
+
 if __name__ == "__main__":
     # use the arg [ debug=True ] to run in debug mode
+    # NOTE: do NOT use [ debug=True ] in production
     app.run(debug=True)
