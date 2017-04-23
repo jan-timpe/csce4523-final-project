d498653a9cd5badd7df93231d061c9822d9ce7a2
Jan Timpe <jantimpe@email.uark.edu>
2017-04-23 09:31:57 -0500

    Removed github link from home, added creator recognition

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
-# import proddb as database # avoid committing creds
-import devdb as database
+import proddb as database # avoid committing creds
+# import devdb as database
 
 db = database.database
 objects = database.objects
